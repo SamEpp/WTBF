ea51adb6d6c8fc0af09e769da8087777ab2b8bbe
Sarah Barden <sarah.barden@students.olin.edu>
2017-05-03 22:12:38 -0400

    fixing a few typos

--- LocationsToMap.py
+++ LocationsToMap.py
@@ -15,8 +15,8 @@ def geocode(place):
     INPUT: a place name as an attribute of a book object
     OUTPUT: a list containing the latitude and longitude for that place
     """
-    geocoder = OpenCageGeocode(geokey)
-    location = geocoder.geocode(place, key=geokey)
+    geocoder = OpenCageGeocode(geo_key)
+    location = geocoder.geocode(place, key=geo_key)
     # We search wikipedia for 3 locations, but don't always get 3.
     # If a location isn't found, it's an empty list, so we do not plot it.
     if location == []:
